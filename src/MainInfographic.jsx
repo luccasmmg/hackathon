1e613d450b02c4f18f925f98213d88251136a17e
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2020-08-30 09:29:57 -0300

    Added infographic

--- src/MainInfographic.jsx
+++ src/MainInfographic.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import terms from "./infographic/Termos.png";
+import infographic from "./infographic/Infografico.png";
 
 const MainInfographic = () => (
   <main className={`my-4 App-main`}>
     <img src={terms} alt="Termos Essenciais LGPD"></img>
+    <img src={infographic} alt="Infografico LGPD"></img>
   </main>
 );
 
