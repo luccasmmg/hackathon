779fe0ad14d0043d976175b884287f1a3c3675ae
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2020-08-30 10:09:56 -0300

    This is not supposed to build

--- src/MainCompany.jsx
+++ src/MainCompany.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import header from "./Headers/HeaderEmpresa.png";
+import survey from "./survey.png";
 import { Link } from "react-router-dom";
 
 const MainCompany = () => (
   <main className={`App-main`}>
     <img src={header} alt="Header Empresa" />
-    <p className={"text-blue-700"}>Teste LGPD</p>
+    <img src={survey} alt="Survey Empresa" />
   </main>
 );
 
